bf8dcaee9052b6157cc288896c41af858adcc730
izumi miyamura <146752758+izumi11-dev@users.noreply.github.com>
2024-07-13 16:01:54 +0630

    Update response.js

--- response.js
+++ response.js
@@ -1,6 +1,6 @@
 const responseObj = {
   HIV:"HIV remains a major global public health issue, having claimed 40.4 million [32.9–51.3 million] lives so far with ongoing transmission in all countries globally; with some countries reporting increasing trends in new infections when previously on the decline.",
-  
+  Sawwinkyaw:"ကိုစော"
 };
 
 
